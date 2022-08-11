b6f1525acc2d6a8f21940e3537463fc1a9b08fb4
tahmid148 <tahmidrahman2812@outlook.com>
2022-08-11 16:06:23 +0100

    feat: added buttons and text to home screen

--- app/src/main/java/com/ntpc/myskills/ui/home/HomeFragment.java
+++ app/src/main/java/com/ntpc/myskills/ui/home/HomeFragment.java
@@ -1,21 +1,25 @@
 package com.ntpc.myskills.ui.home;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.ntpc.myskills.R;
 import com.ntpc.myskills.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
 
+    @SuppressLint("SetTextI18n")
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         HomeViewModel homeViewModel =
@@ -24,8 +28,30 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        TextView desc = root.findViewById(R.id.description);
+        desc.setText("Whether you are looking for a great career, to enhance\n" +
+                     " your current skills to be competitive in our local,\n" +
+                     " regional, or global society, I am certain that you will \n" +
+                     "find enrolling at National Training and Productivity\n" +
+                     " Centre to be one of your best decisions. NTPC offers \n" +
+                     "a wide range of programs including market-relevant \n" +
+                     "stackable certificates, diplomas and QuickJob \n" +
+                     "programs designed to help you rapidly meet your \n" +
+                     "career and personal goals. You will find that NTPC \n" +
+                     "offers specific training programs that translate into \n" +
+                     "lifelong opportunities.");
+
+        Button courses = root.findViewById(R.id.view_courses_btn);
+        Button enrol = root.findViewById(R.id.enrol_btn);
+        Button calendar = root.findViewById(R.id.view_calendar_btn);
+        Button contact = root.findViewById(R.id.contact_btn);
+
+        courses.setText("View Courses");
+        enrol.setText("Enrol");
+        calendar.setText("View Calendar");
+        contact.setText("Contact Us");
+
+
         return root;
     }
 
